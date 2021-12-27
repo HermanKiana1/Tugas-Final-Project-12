fa44c370d734c03af75a6ec65bd7076781f7d8a3
KB-FMF-KPM <kb-fmf-kpm@KB-FMF-KPMs-MacBook-Pro.local>
2021-12-27 09:35:40 +0700

    first commit

---
+++ Test Suites/Login Bank 2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Bank 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ded0f04b-2175-479e-bcb7-6d1f772eed46</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e93a502-9347-4212-80be-4668e1a9096d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Bank</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
